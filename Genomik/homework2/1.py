15fd6db1ccdcc4682bf91ad2c8f59cc4c51fbed5
Xuesong Wang <wangxuesong29@gmail.com>
2017-05-09 11:02:22 +0200

    basic function

---
+++ Genomik/homework2/1.py
@@ -0,0 +1,61 @@
+#- *- coding: UTF-8 -*-
+
+import sys
+
+inputNum = int(sys.argv[1])
+
+print "The number for the SAM bit flag is ", inputNum
+brinaryinputNum = bin(inputNum)
+print "The brianry is  ", brinaryinputNum
+
+bits = 0
+temp = inputNum
+while(inputNum != 0):
+	inputNum = inputNum >> 1
+	bits += 1
+
+inputNum = temp
+flagBits = []
+
+for i in range(bits):
+	if(inputNum%2 == 0):
+		flagBits.append(0)
+		inputNum = inputNum >> 1
+	else:
+		flagBits.append(1)
+		inputNum = inputNum >> 1
+
+explain = ["0x1    template having multiple segments in sequencing",
+           "0x2    each segment properly aligned according to the aligner",
+           "0x4    segment unmapped",
+           "0x8    next segment in the template unmapped",
+           "0x10   SEQ being reverse complemented",
+           "0x20   SEQ of the next segment in the template being reversed 0x40 "
+           "the first segment in the template",
+           "0x80   the last segment in the template",
+           "0x100  secondary alignment",
+           "0x200  not passing quality controls",
+           "0x400  PCR or optical duplicate",
+           "0x800  suppleme"
+           ]
+i = 0
+for i in range(bits):
+	if(flagBits[i] == 1):
+		print explain[i]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
