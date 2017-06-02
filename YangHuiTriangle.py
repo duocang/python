43053d3d59bfa1708e1c1eba37d1f8eb3d239bf3
Xuesong Wang <wangxuesong29@gmail.com>
2017-06-03 00:26:00 +0200

    closure

--- YangHuiTriangle.py
+++ YangHuiTriangle.py
@@ -44,3 +44,13 @@ for n in primes():
         print(n)
     else:
         break
+
+def count():
+    fs = []
+    for i in range(1, 4):
+        def f():
+             return i*i
+        fs.append(f)
+    return fs
+
+f1, f2, f3 = count()
