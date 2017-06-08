69265b6dceab17e5e9d49af84982b8aa89c2e932
Xuesong Wang <wangxuesong29@gmail.com>
2017-06-08 09:54:11 +0200

    doctest

--- learning/unitTest.py
+++ learning/unitTest.py
@@ -1,19 +1,44 @@
 # -*-coding: utf-8 -*-
 import unittest
+import doctest
 
 class Dict(dict):
+    '''
+    Simple dict but also support access as x.y style.
+    >>> d1 = Dict()
+    >>> d1['x'] = 100
+    >>> d1.x
+    100
+    >>> d1.y = 200
+    >>> d1['y']
+    200
+    >>> d2 = Dict(a=1, b=2, c='3')
+    >>> d2.c
+    '3'
+    >>> d2['empty']
+    Traceback (most recent call last):
+        ...
+    KeyError: 'empty'
+    >>> d2.empty
+    Traceback (most recent call last):
+        ...
+    AttributeError: 'Dict' object has no attribute 'empty'
+        '''
 
     def __init__(self, **kw):
-        super().__init__(**kw)
+        super(Dict, self).__init__(**kw)
 
     def __getattr__(self, key):
         try:
             return self[key]
         except KeyError:
-            raise AttributeError("Dict object had no attribute %s" % key)
+            raise AttributeError(r"'Dict' object has no attribute '%s'" % key)
+
     def __setattr__(self, key, value):
         self[key] = value
 
+if __name__ == '__main__':
+    doctest.testmod()
 #d = Dict(a=1, b=2)
 #d['a']
 ## 1
@@ -23,8 +48,10 @@ class Dict(dict):
 class TestDict(unittest.TestCase):
     def setUp(self):
         print("这里是setUP")
+
     def tearDown(self):
         print("这里是tearDown")
+
     def test_init(self):
         d = Dict(a=1, b='test')
         self.assertEqual(d.a, 1)
@@ -51,6 +78,7 @@ class TestDict(unittest.TestCase):
         d = Dict()
         with self.assertRaises(AttributeError):
             value = d.empty
+
     if __name__ == '__main__':
         unittest.main()
         test_attr()
