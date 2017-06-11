86a84f10732e83087b2509fc13864149a197989a
Xuesong Wang <wangxuesong29@gmail.com>
2017-06-11 21:57:48 +0200

    udp

---
+++ learning/UDPTest/server.py
@@ -0,0 +1,15 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# 绑定端口
+s.bind(('127.0.0.1', 9999))
+
+print('Bind UPD on 9999')
+while True:
+    # 接受数据
+    # recvfrom()方法返回数据和客户端的地址与端口，这样，服务器收到数据后，直接调用sendto()就可以把数据用UDP发给客户端
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    s.sendto(b'Hello, %s!' % data, addr)
+
+
