75d6f79dd5c7236a6ad3c4efbcb5e07267f0a190
Xuesong Wang <wangxuesong29@gmail.com>
2017-06-18 21:58:16 +0200

    plot data

--- TensorFlow/neuralNetwork.py
+++ TensorFlow/neuralNetwork.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 def add_layer(inputs, in_size, out_size, activation_function=None):
 	Weights = tf.Variable(tf.random_normal([in_size, out_size]))
@@ -30,7 +31,21 @@ init = tf.initialize_all_variables()
 sess = tf.Session() # 可以理解为抽象模型的实现者
 sess.run(init)
 
+fig = plt.figure()  # create a fig frame
+ax = fig.add_subplot(1, 1, 1)
+ax.scatter(x_data, y_data)
+plt.ion()
+plt.show()
+
 for i in range(1000):
-	sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
-	if i % 10 == 0:
-		print(sess.run(loss), feed_dict={xs:x_data, ys:y_data})
+    sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+    if i % 10 == 0:
+        #print(sess.run(loss), feed_dict={xs:x_data, ys:y_data})
+        try:
+            ax.lines.remove(lines[0])
+        except Exception:
+            pass
+        predition_value = sess.run(predition, feed_dict={xs:x_data})
+        lines = ax.plot(x_data, predition_value, 'r-', lw=5)
+        plt.pause(0.3 )
+
