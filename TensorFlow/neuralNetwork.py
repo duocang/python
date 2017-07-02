24573a17ddf3cd26a6d5030a76547dd479c77e2b
Xuesong Wang <wangxuesong29@gmail.com>
2017-07-02 17:41:56 +0200

    more graphics of loss and weights

--- TensorFlow/neuralNetwork.py
+++ TensorFlow/neuralNetwork.py
@@ -5,20 +5,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def add_layer(inputs, in_size, out_size, activation_function=None):
+def add_layer(inputs, in_size, out_size, n_layer, activation_function=None):
     # add one more layer and return the output of this layer
+    layer_name = 'layer%s' % n_layer
     with tf.name_scope("layer"):
         with tf.name_scope("weights"):
             Weights = tf.Variable(tf.random_normal([in_size, out_size]), name='W')
+            tf.summary.histogram(layer_name + '/weights', Weights)
         with tf.name_scope("biases"):
-	        biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            biases = tf.Variable(tf.zeros([1, out_size]) + 0.1, name='b')
+            tf.summary.histogram(layer_name + '/biases', biases)
         with tf.name_scope("Wx_plus_b"):
             Wx_plus_b = tf.add(tf.matmul(inputs, Weights), biases)
         if activation_function is None:
             outputs = Wx_plus_b
         else:
             outputs = activation_function(Wx_plus_b, )
-        return outputs
+        tf.summary.histogram(layer_name + '/outputs', outputs)
+    return outputs
 
 # define placeholder for inputs to network
 with tf.name_scope("inputs"):
@@ -27,9 +31,9 @@ with tf.name_scope("inputs"):
 
 
 # add a hidden layer
-l1 = add_layer(xs, 1, 10, activation_function=tf.nn.relu)
+l1 = add_layer(xs, 1, 10, 1, activation_function=tf.nn.relu)
 # add output layer
-predition = add_layer(l1, 10, 1, activation_function=None)
+predition = add_layer(l1, 10, 1, 2, activation_function=None)
 
 # the error between prediciton and real data
 with tf.name_scope('loss'):
@@ -39,6 +43,9 @@ with tf.name_scope('train'):
     train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
 
 sess = tf.Session() # 可以理解为抽象模型的实现者
+merged = tf.summary.merge_all()
+
+writer = tf.summary.FileWriter("logs/", sess.graph)
 
 # tf.train.SummaryWriter soon be deprecated, use following
 if int((tf.__version__).split('.')[1]) < 12 and int((tf.__version__).split('.')[0]) < 1:  # tensorflow version < 0.12
@@ -84,3 +91,13 @@ for i in range(1000):
 
 # how to run the code
 # tensorboard --logdir="logs"
+
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs: x_data, ys: y_data})
+    if i % 50 == 0:
+        result = sess.run(merged,
+                          feed_dict={xs: x_data, ys: y_data})
+        writer.add_summary(result, i)
+
+# direct to the local dir and run this in terminal:
+# $ tensorboard --logdir logs
